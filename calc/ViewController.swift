89be3f407638900934693978bdfff855f7b90fae
Alexandr Kolosovsky <kolo-ky@yandex.ru>
2020-12-28 14:28:23 +0500

    first commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by ALEKSANDR KOLOSOVSKIY on 28.12.2020.
+//  Copyright © 2020 ALEKSANDR KOLOSOVSKIY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
